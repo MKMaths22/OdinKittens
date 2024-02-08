96681836f73ecaddc342ca1153224ae52eb73c65
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-02-08 15:57:39 +0000

    Create class method in Kitten for self.cuteness_values so that these options are used for dropdown menu in new view. Modify seeds to have just three different cuteness and three different softness values. Write most of #new form and dummy #edit view.

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,14 @@
 class Kitten < ApplicationRecord
+  def 
+    self.cuteness_values
+    ['adorable', 'average', 'extremely cute']
+  end
+  
+  def
+    self.softness_values 
+    ['average', 'fluffy', 'very fluffy']
+  end
+  
+  validates :cuteness, :inclusion => cuteness_values
+  validates :softness, :inclusion => softness_values
 end
