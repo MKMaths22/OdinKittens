8ef1bb596c4603d584a434df2f1e61eb4ca930e3
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-02-08 14:59:16 +0000

    Flesh out Kittens controller methods. Make #index and #show views, using _display_kitten partial.

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,22 +1,46 @@
 class KittensController < ApplicationController
   def index
+    @kittens = Kitten.all
   end
 
   def show
+    @kitten = Kitten.find(params[:id])
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.new(allowed_params)
+    if @kitten.save
+      redirect_to kitten_path(id: @kitten.id)
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def update
+    if @kitten.update(allowed_params)
+      redirect_to kitten_path(id: @kitten.id)
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    redirect_to 'root'
+  end
+
+  private
+
+  def allowed_params
+    params.require(kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
